da22184d32642b93326bfb59f9f6d0da6cc0c595
MahmoudAbdelazim <mahmoud2011989@gmail.com>
2023-07-03 19:03:41 +0300

    Update footer link

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,7 +3,16 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <p>2023 Copyrights &copy; Mahmoud Abdelazim</p>
+      <p>
+        2023 Copyrights &copy;{" "}
+        <a
+          className={styles.link}
+          href="https://www.linkedin.com/in/mahmoudabdelazim/"
+          target={"_blank"}
+        >
+          Mahmoud Abdelazim
+        </a>
+      </p>
     </div>
   );
 }
