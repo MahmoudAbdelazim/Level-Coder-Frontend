c7e318b24d49c3e863adf44730a89ab9e8b4d5be
MahmoudAbdelazim <mahmoud2011989@gmail.com>
2023-07-05 01:37:20 +0300

    Add static pages

--- pages/topic/[name].js
+++ pages/topic/[name].js
@@ -5,42 +5,82 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
-export default function TopicPage() {
+export async function getStaticPaths() {
+  try {
+    const res = await fetch(
+      process.env.NEXT_PUBLIC_BACKEND_URL + "/topic/all-topics"
+    );
+    const data = await res.json();
+    const topics = data.topics;
+    const paths = topics.map((topic) => ({
+      params: {
+        name: topic.name,
+      },
+    }));
+    return { paths, fallback: true };
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const name = params.name;
+  var myHeaders = new Headers();
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_BACKEND_URL + "/topic/topic-by-name/" + name,
+    requestOptions
+  );
+  const result = await response.json();
+  let fetchedTopic = result.topic;
+  fetchedTopic = calculatePercentages(fetchedTopic);
+  return {
+    props: {
+      fetchedTopic: fetchedTopic,
+    },
+  };
+}
+
+const calculatePercentages = (fetchedTopic) => {
+  let solvedCfCount = 0,
+    solvedLcCount = 0,
+    solvedHrCount = 0;
+  for (const problem of fetchedTopic.cfProblems) {
+    if (problem.solved) solvedCfCount++;
+  }
+  for (const problem of fetchedTopic.lcProblems) {
+    if (problem.solved) solvedLcCount++;
+  }
+  for (const problem of fetchedTopic.hrProblems) {
+    if (problem.solved) solvedHrCount++;
+  }
+  fetchedTopic.cfSolved = solvedCfCount;
+  fetchedTopic.lcSolved = solvedLcCount;
+  fetchedTopic.hrSolved = solvedHrCount;
+  fetchedTopic.cfPercentage =
+    solvedCfCount == 0
+      ? 0
+      : Math.floor((solvedCfCount / fetchedTopic.cfProblems.length) * 100);
+  fetchedTopic.lcPercentage =
+    solvedLcCount == 0
+      ? 0
+      : Math.floor((solvedLcCount / fetchedTopic.lcProblems.length) * 100);
+  fetchedTopic.hrPercentage =
+    solvedHrCount == 0
+      ? 0
+      : Math.floor((solvedHrCount / fetchedTopic.hrProblems.length) * 100);
+  return fetchedTopic;
+};
+
+export default function TopicPage({ fetchedTopic }) {
   const [topic, setTopic] = useState({});
   const [loading, setLoading] = useState(false);
   const router = useRouter();
   const { name } = router.query;
-
-  const calculatePercentages = (fetchedTopic) => {
-    let solvedCfCount = 0,
-      solvedLcCount = 0,
-      solvedHrCount = 0;
-    for (const problem of fetchedTopic.cfProblems) {
-      if (problem.solved) solvedCfCount++;
-    }
-    for (const problem of fetchedTopic.lcProblems) {
-      if (problem.solved) solvedLcCount++;
-    }
-    for (const problem of fetchedTopic.hrProblems) {
-      if (problem.solved) solvedHrCount++;
-    }
-    fetchedTopic.cfSolved = solvedCfCount;
-    fetchedTopic.lcSolved = solvedLcCount;
-    fetchedTopic.hrSolved = solvedHrCount;
-    fetchedTopic.cfPercentage =
-      solvedCfCount == 0
-        ? 0
-        : Math.floor((solvedCfCount / fetchedTopic.cfProblems.length) * 100);
-    fetchedTopic.lcPercentage =
-      solvedLcCount == 0
-        ? 0
-        : Math.floor((solvedLcCount / fetchedTopic.lcProblems.length) * 100);
-    fetchedTopic.hrPercentage =
-      solvedHrCount == 0
-        ? 0
-        : Math.floor((solvedHrCount / fetchedTopic.hrProblems.length) * 100);
-    return fetchedTopic;
-  };
 
   const fetchTopic = async () => {
     setLoading(true);
@@ -93,7 +133,7 @@ export default function TopicPage() {
         </>
       )}
       <Head>
-        <title>{topic?.name} - Level Coder</title>
+        <title>{fetchedTopic?.name} - Level Coder</title>
       </Head>
       <NavBar />
       <div className="main">
